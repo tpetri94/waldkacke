e8cedb125ffc8b74c9cfd4ecdf5713a461fa37c8
Tobias Petri <tobias.petri94@gmail.com>
2016-12-01 17:23:06 +0100

    Camera Controllör added

--- GreenPlanet/Assets/Scripts/CameraController.cs
+++ GreenPlanet/Assets/Scripts/CameraController.cs
@@ -1,37 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class CameraController : MonoBehaviour {
 
     public int moveSpeed;
     public int rotationSpeed;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 10F;
+    public float sensitivityY = 10F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationX = 0F;
+    float rotationY = 0F;
+    Quaternion originalRotation;
 
-    private Camera me;
+    //private Camera me;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        me = GetComponent<Camera>();
-	}
+        //me = GetComponent<Camera>();
+        // Make the rigid body not change rotation
+        originalRotation = transform.localRotation;
+    }
 
-	// Update is called once per frame
-	void FixedUpdate ()
+	// Updas called once per frame
+	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+
+        if (axes == RotationAxes.MouseXAndY)
         {
-            me.gameObject.transform.Translate(Vector3.forward * moveSpeed);
+            // Read the mouse input axis
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (axes == RotationAxes.MouseX)
         {
-            me.gameObject.transform.Translate(-Vector3.forward * moveSpeed);
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        else
         {
-            me.gameObject.transform.rotation = Quaternion.Euler(Vector3.up * -rotationSpeed);
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            transform.localRotation = originalRotation * yQuaternion;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+
+        if (Input.GetKey(KeyCode.W))
         {
-            me.gameObject.transform.rotation = Quaternion.Euler(Vector3.up * rotationSpeed);
+            gameObject.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+       /* if (angle & lt; -360F)
+         angle += 360F;
+        if (angle & gt; 360F)
+         angle -= 360F;*/
+        return Mathf.Clamp(angle, min, max);
     }
 }
